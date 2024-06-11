426ed9c814b8b2d9e721da2453d8adbee61a996e
Salah AlDin <salahqerem@gmail.com>
2024-06-11 11:33:12 +0300

    Safer-5 Setup Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { Box } from "@mui/material";
 import LandingPage from "@/pages/LandingPage";
-import SaferThemeProvider from "@/style/SaferThemeProvider";
+import Providers from "./Providers";
 
 function App() {
   return (
-    <SaferThemeProvider>
-      <Box sx={{ bgcolor: "background.default" }}>
-        <LandingPage />
-      </Box>
-    </SaferThemeProvider>
+    <Providers>
+      <LandingPage />
+    </Providers>
   );
 }
 
--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,8 +1,20 @@
 import { PropsWithChildren } from "react";
 import SaferThemeProvider from "./style/SaferThemeProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import saferQueryClient from "@/config/TanstackQuery/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }: PropsWithChildren) => {
-  return <SaferThemeProvider>{children}</SaferThemeProvider>;
+  return (
+    <QueryClientProvider client={saferQueryClient}>
+      <SaferThemeProvider>{children}</SaferThemeProvider>
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="left"
+        buttonPosition="bottom-left"
+      />
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
