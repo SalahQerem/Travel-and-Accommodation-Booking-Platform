b7fb9d520ec1f6bbf68a713f6d0e5adc4abe410e
Salah AlDin <salahqerem@gmail.com>
2024-06-12 19:30:32 +0300

    Safer-9 Add Routing Authentication

--- src/components/Snackbar/Alert.tsx
+++ src/components/Snackbar/Alert.tsx
@@ -1,10 +1,10 @@
 import Alert, { AlertProps } from "@mui/material/Alert";
 import { forwardRef } from "react";
 
-const AnharAlert = forwardRef<HTMLDivElement, AlertProps>((props, ref) => {
+const SaferAlert = forwardRef<HTMLDivElement, AlertProps>((props, ref) => {
   return <Alert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-AnharAlert.displayName = "Alert";
+SaferAlert.displayName = "Alert";
 
-export default AnharAlert;
+export default SaferAlert;
