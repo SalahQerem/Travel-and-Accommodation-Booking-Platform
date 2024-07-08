935938a85c6df8c8238b2c884e5669a69cffa714
Salah AlDin <salahqerem@gmail.com>
2024-07-08 21:16:32 +0300

    Safer-19 Implement the Search for City functionality

--- src/pages/Admin/Cities/Cities.tsx
+++ src/pages/Admin/Cities/Cities.tsx
@@ -2,21 +2,31 @@ import { defaultRequestQuery } from "@/constants";
 import BlockUI from "@/containers/BlockUI";
 import StyledContainer from "@/containers/StyledContainer";
 import routeHOC from "@/routes/HOCs/routeHOC";
-import { City as CityType } from "@/types";
-import { Button, Container, Grid, Stack, Typography } from "@mui/material";
-import { useState } from "react";
+import { City as CityType, RequestQuery } from "@/types";
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ChangeEvent, useState } from "react";
 import AddCityDialog from "./components/AddCityDialog";
 import City from "./components/City";
 import { defaultCity } from "./constants";
 import useGetCitiesAPI from "./hooks/useGetCitiesAPI";
+import { Plus, Search } from "lucide-react";
 
 const Cities = () => {
   const [isCityFormDialogOpen, setIsCityFormDialogOpen] =
     useState<boolean>(false);
   const [cityToUpdate, setCityToUpdate] = useState<CityType>(defaultCity);
+  const [nameToSearch, setNameToSearch] = useState<string>("");
+  const [requestQuery, setRequestQuery] =
+    useState<RequestQuery>(defaultRequestQuery);
 
-  const { cities, refetchCities, isFetching } =
-    useGetCitiesAPI(defaultRequestQuery);
+  const { cities, refetchCities, isFetching } = useGetCitiesAPI(requestQuery);
 
   const handleOpenCityFormDialog = () => {
     setIsCityFormDialogOpen(true);
@@ -28,6 +38,10 @@ const Cities = () => {
   const handleUpdateCity = (city: CityType) => {
     setIsCityFormDialogOpen(true);
     setCityToUpdate(city);
+  };
+
+  const handleSearchForCity = () => {
+    setRequestQuery({ ...requestQuery, name: nameToSearch });
   };
 
   const renderCities = cities?.map((city) => (
@@ -54,13 +68,32 @@ const Cities = () => {
             <Typography variant="h4" component="h1">
               Cities
             </Typography>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleOpenCityFormDialog}
-            >
-              Add City
-            </Button>
+            <Stack direction="row" gap={2}>
+              <TextField
+                placeholder="Search by name"
+                value={nameToSearch}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setNameToSearch(event.target.value)
+                }
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSearchForCity}
+                disabled={requestQuery.name === nameToSearch}
+                endIcon={<Search />}
+              >
+                Search
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleOpenCityFormDialog}
+                endIcon={<Plus />}
+              >
+                Add City
+              </Button>
+            </Stack>
           </Stack>
           <Grid container spacing={2}>
             {renderCities}
